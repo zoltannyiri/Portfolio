e797dbe3a4278546aab57872d829dfad2d37e1db
Nyiri Zoltán <zoltan.nyiri02@gmail.com>
2026-08-17 15:06:27 +0200

    update contactscreen

--- web/src/pages/ContactScreen/ContactScreen.jsx
+++ web/src/pages/ContactScreen/ContactScreen.jsx
@@ -84,6 +84,23 @@ const ContactScreen = () => {
               </div>
             </div>
 
+            <div className="mt-5 rounded-xl border border-gray-700 opacity bg-[#162029] px-6 py-6">
+              <div className="flex items-center gap-5">
+                <span className="relative flex h-3 w-3">
+                  <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
+                  <span className="relative inline-flex h-3 w-3 rounded-full bg-green-500"></span>
+                </span>
+
+                <span className="text-white font-bold">
+                  Nyitott vagyok új lehetőségekre
+                </span>
+              </div>
+              
+              <div className="text-gray-400 text-sm mt-3">
+                Teljes állású vagy projekt alapú együttműködés
+              </div>
+            </div>
+
           </div>
 
           <div className="md:grid-cols-2">
